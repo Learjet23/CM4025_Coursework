214e74a4d4071e62b79a00a9d537b3acaec71033
scott.learjet@gmail.com <Kirsty_23>
2018-04-17 18:08:27 +0100

    update

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 <html>
 <head>
     <title>Wizard Duels</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="/CSS/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div id="header">1</div>
     <div id="container">
-        <div id="navigation">2</div>
-        <div id="content">3</div>
+        <div id="navigation">2<br /> <br /></div>
+        <div id="content">3<br /> <br /> <br /></div>
     </div>
 <div id="footer">4</div>
 </body>
