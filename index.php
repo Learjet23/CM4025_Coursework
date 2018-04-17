83e3eaefece39ab0902e15312bf6b01c72d2e2f4
scott.learjet@gmail.com <Kirsty_23>
2018-04-17 17:02:56 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scott
+ * Date: 17/04/2018
+ * Time: 16:50
+ */
+?>
+<html>
+<head>
+    <title>Wizard Duels</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="header">1</div>
+    <div id="container">
+        <div id="navigation">2</div>
+        <div id="content">3</div>
+    </div>
+<div id="footer">4</div>
+</body>
+</html>
