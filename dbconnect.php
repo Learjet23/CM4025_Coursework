e8666c1386abb8788c52011332c06885ca9e6cc6
scott.learjet@gmail.com <Kirsty_23>
2018-04-17 19:11:34 +0100

    dbconnect

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scott
+ * Date: 17/04/2018
+ * Time: 18:12
+ */
+define('DB_SERVER', 'cm4025-wizard.mysql.database.azure.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'Admin_123');
+define('DB_DATABASE', 'mysql');
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
