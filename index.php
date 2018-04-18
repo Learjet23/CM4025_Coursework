2cd36ed71c9c6726d07e615db93bdb594c5a8ea8
scott.learjet@gmail.com <Kirsty_23>
2018-04-19 00:28:08 +0100

    Register User

--- index.php
+++ index.php
@@ -49,6 +49,6 @@ if (isset($_GET['logout'])) {
         <div id="navigation">2<br /> <br /></div>
         <div id="content">3<br /> <br /> <br /></div>
     </div>
-<div id="footer">&copy; Wizard Duel 2018: A Scott Learmonth Game</div>
+    <div id="footer"><center>&copy; Wizard Duel 2018: A Scott Learmonth Game</center></div>
 </body>
 </html>
