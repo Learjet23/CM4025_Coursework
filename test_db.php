f4bcb2fb4d565fbd991309df117655a53f34a27c
scott.learjet@gmail.com <Kirsty_23>
2018-04-18 21:09:14 +0100

    test dbconnect

--- test_db.php
+++ test_db.php
@@ -17,7 +17,7 @@ echo "\nbefore executing query";
 if (mysqli_query($db, $sql)) {
     echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $db->error;
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
 }
 echo "\n after query";
 $db->close();
