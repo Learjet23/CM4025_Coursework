b965683a9b73bafb8aed79af841629a530859a17
scott.learjet@gmail.com <Kirsty_23>
2018-04-18 15:04:11 +0100

    test dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -5,9 +5,9 @@
  * Date: 17/04/2018
  * Time: 18:12
  */
-define('DB_SERVER', 'cm4025-wizard.mysql.database.azure.com');
-define('DB_USERNAME', 'admindb');
-define('DB_PASSWORD', 'Admin_123');
-define('DB_DATABASE', 'wizard');
+$host = 'cm4025-wizard.mysql.database.azure.com';
+$user = 'admindb';
+$pwd = 'Admin_123';
+$data = 'wizard';
 
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$db = mysqli_connect($host, $user, $pwd, $data);
