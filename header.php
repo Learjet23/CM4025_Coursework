4d8a2a76e1450628113eb7eba3972eda6c8ac988
scott.learjet@gmail.com <Kirsty_23>
2018-04-19 21:49:11 +0100

    Register User

--- header.php
+++ header.php
@@ -1,6 +1,3 @@
-<?php
-include ('player.php');
-?>
 <html>
 <head>
     <title>Wizard Duels</title>
@@ -8,27 +5,26 @@ include ('player.php');
 </head>
 <body>
 <div class="content">
-    <!-- notification message -->
-    <?php if (isset($_SESSION['success'])) : ?>
-        <div class="error success" >
-            <h3>
-                <?php
-                echo $_SESSION['success'];
-                unset($_SESSION['success']);
-                ?>
-            </h3>
-        </div>
-    <?php endif ?>
-
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
         <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-        <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 </div>
 
 <!--Game box-->
 <div id="header">Wizard Duel</div>
 <div id="container">
-    <div id="navigation"><div id="nav_div"><?php ?></div></div>
+    <div id="navigation">
+        <div id="nav_div">
+            <?php
+                if(isset($_SESSION['username'])){
+                    include ("player.php");
+                }
+            ?>
+            &raquo; <a href="spells.php">Your spells</a><br/><br/>
+            &raquo; <a href="ranks.php">Rankings</a><br/><br/>
+            &raquo; <a href="stats.php">Your stats</a><br/><br/>
+            &raquo; <a href="index.php?logout='1'" style="color: red;">logout</a>
+        </div>
+    </div>
     <div id="content"><div id="con_div">
