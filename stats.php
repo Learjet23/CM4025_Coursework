35f8dc1bbb0c71b0e46a3e89e7e3e643345ef957
scott.learjet@gmail.com <Kirsty_23>
2018-04-20 12:56:43 +0100

    stats

--- stats.php
+++ stats.php
@@ -18,7 +18,15 @@ if(!isset($_SESSION['username'])){
     <table>
         <tr>
             <td>Username: </td>
-            <td><i><?php echo $user_get['username']; ?></i></td>
+            <td><i>
+                <?php
+                if (mysqli_num_rows($usr_spells_get) > 0) {
+                    while($row = mysqli_fetch_assoc($usr_spells_get)) {
+                        echo $row["username"]. "<br>";
+                    }
+                }
+                ?>
+            </i></td>
         </tr>
     </table>
 <?php
