c1b4983b578338fda83d8ae4426b68bacb7d0a50
scott.learjet@gmail.com <Kirsty_23>
2018-04-23 11:05:16 +0100

    stats

--- safe.php
+++ safe.php
@@ -9,13 +9,16 @@
 
 $users_sql = "SELECT * FROM users WHERE username='".$_SESSION['username']."'";
 $users_get = mysqli_query($db, $users_sql);
+$users = mysqli_fetch_assoc($users_get);
 
-$spells = "SELECT * FROM spells";
-$spells_get = mysqli_query($db, $spells);
+$spells_sql = "SELECT * FROM spells";
+$spells_get = mysqli_query($db, $spells_sql);
+$spells = mysqli_fetch_assoc($spells_get);
 
-$usrid_get = "SELECT iduser FROM users WHERE username='".$_SESSION['username']."'";
-$userid = mysqli_query($db, $usrid_get);
+$usrid_sql = "SELECT iduser FROM users WHERE username='".$_SESSION['username']."'";
+$userid_get = mysqli_query($db, $usrid_sql);
+$userid = mysqli_fetch_assoc($users_get);
 
-$usr_spells = "SELECT * FROM usr_spells WHERE userid='$userid'";
-$usr_spells_get = mysqli_query($db, $usr_spells);
-?>
+$usr_spells_sql = "SELECT * FROM usr_spells WHERE userid='$userid'";
+$usr_spells_get = mysqli_query($db, $usr_spells_sql);
+$usr_spells = mysqli_fetch_assoc($usr_spells_get);
