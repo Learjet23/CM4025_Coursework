81f6f2f4ac8d5fd52ea435ffcecbaf0418b40e69
scott.learjet@gmail.com <Kirsty_23>
2018-04-23 10:40:13 +0100

    stats

--- safe.php
+++ safe.php
@@ -6,12 +6,15 @@
  * Time: 00:52
  */
 
+
 $users_sql = "SELECT * FROM users WHERE username='".$_SESSION['username']."'";
 $users_get = mysqli_query($db, $users_sql);
 
 $spells = "SELECT * FROM spells";
 $spells_get = mysqli_query($db, $spells);
 
-$userid = $users_get['userid'];
+$usrid_get = "SELECT userid FROM users WHERE username='".$_SESSION['username']."'";
+$userid = mysqli_query($db, $usrid_get);
+
 $usr_spells = "SELECT * FROM usr_spells WHERE userid='$userid'";
 $usr_spells_get = mysqli_query($db, $usr_spells);
--- stats.php
+++ stats.php
@@ -7,19 +7,10 @@
  */
 session_start();
 include ("header.php");
+include ("safe.php");
+
 $username = $_SESSION['username'];
 
-$users_sql = "SELECT * FROM users WHERE username='".$_SESSION['username']."'";
-$users_get = mysqli_query($db, $users_sql);
-
-$spells = "SELECT * FROM spells";
-$spells_get = mysqli_query($db, $spells);
-
-$usrid_get = "SELECT userid FROM users WHERE username='".$_SESSION['username']."'";
-$userid = mysqli_query($db, $usrid_get);
-
-$usr_spells = "SELECT * FROM usr_spells WHERE userid='$userid'";
-$usr_spells_get = mysqli_query($db, $usr_spells);
 
 if(!isset($_SESSION['username'])){
     echo "You must be logged in to view this page";
