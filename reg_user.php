5762ee21c6aafc2e869f0d98b4cc7805072f1db0
scott.learjet@gmail.com <Kirsty_23>
2018-04-30 00:41:19 +0100

    spells

--- reg_user.php
+++ reg_user.php
@@ -42,6 +42,7 @@ if(isset($_POST['reg_user'])){
         $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
         mysqli_query($db, $sql);
         $stat_sql = "INSERT INTO usr_spells (expel, avada, crucio, flipendo) VALUES ('70','1','10','100') WHERE iduser='".$users['iduser']."'";
+        mysqli_query($db, $stat_sql);
         $_SESSION['username'] = $username;
         $_SESSION['login_date'] = $current_date;
         $_SESSION['success'] = "You are logged in";
