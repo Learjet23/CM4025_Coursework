c1c0d954aee60e790702dd7201dd9ddae93ef3ea
scott.learjet@gmail.com <Kirsty_23>
2018-04-30 02:38:05 +0100

    stats

--- stats.php
+++ stats.php
@@ -11,13 +11,13 @@ if(!isset($_SESSION['username'])){
     echo "You must be logged in to view this page";
 }else{
     if(!isset($_GET['id'])){
-        output("YOU SHOULD NOT BE HERE");
+        echo "YOU SHOULD NOT BE HERE";
     }else{
         $id = protect($_GET['id']);
         $usr_check_sql = "SELECT * FROM users WHERE iduser='". $id ."'";
         $usr_check = mysqli_query($db, $usr_check_sql);
         if(mysqli_num_rows($usr_check) == 0){
-            output("NO USER WITH THIS ID");
+            echo "NO USER WITH THIS ID";
         }else{
 
 
