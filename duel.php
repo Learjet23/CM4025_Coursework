34a72acb13e7cfa693507adf045651b4b7f21071
scott.learjet@gmail.com <Kirsty_23>
2018-05-01 13:15:00 +0100

    fix xp update

--- duel.php
+++ duel.php
@@ -38,6 +38,7 @@ if(!isset($_SESSION['username'])){
                         mysqli_query($db, $attack_used);
                         $log_sql = "INSERT INTO logs (attackid, defendid, att_dam, def_hp, time) VALUES ('".$users['iduser']."', '$id', '$damage', '".$enemy_stats['HP']."','".time()."')";
                         mysqli_query($db, $log_sql);
+                        $enemy_stats = mysqli_fetch_assoc($usr_check);
                         if ($enemy_stats['HP'] == 0) {
                             echo "Enemy defeated! +100 XP";
                             $xp_update = "UPDATE users SET XP=XP + 100 WHERE iduser='" . $users['iduser'] . "'";
@@ -87,7 +88,6 @@ if(!isset($_SESSION['username'])){
                         $log_sql = "INSERT INTO logs (attackid, defendid, att_dam, def_hp, time) VALUES ('".$users['iduser']."', '$id', '$damage', '".$enemy_stats['HP']."','".time()."')";
                         mysqli_query($db, $log_sql);
                         $enemy_stats = mysqli_fetch_assoc($usr_check);
-                        //echo "Attack hit! " . $enemy_stats['username'] . " has " . $enemy_stats['HP'] . " HP left.";
                         if ($enemy_stats['HP'] == 0) {
                             echo "Enemy defeated! +100 XP";
                             $xp_update = "UPDATE users SET XP=XP + 100 WHERE iduser='" . $users['iduser'] . "'";
@@ -136,7 +136,7 @@ if(!isset($_SESSION['username'])){
                         mysqli_query($db, $attack_used);
                         $log_sql = "INSERT INTO logs (attackid, defendid, att_dam, def_hp, time) VALUES ('".$users['iduser']."', '$id', '$damage', '".$enemy_stats['HP']."','".time()."')";
                         mysqli_query($db, $log_sql);
-                        //echo "Attack hit! " . $enemy_stats['username'] . " has " . $enemy_stats['HP'] . " HP left.";
+                        $enemy_stats = mysqli_fetch_assoc($usr_check);
                         if ($enemy_stats['HP'] == 0) {
                             echo "Enemy defeated! +100 XP";
                             $xp_update = "UPDATE users SET XP=XP + 100 WHERE iduser='" . $users['iduser'] . "'";
@@ -185,7 +185,7 @@ if(!isset($_SESSION['username'])){
                         mysqli_query($db, $attack_used);
                         $log_sql = "INSERT INTO logs (attackid, defendid, att_dam, def_hp, time) VALUES ('" . $users['iduser'] . "','$id', '$damage', '" . $enemy_stats['HP'] . "','" . time() . "')";
                         mysqli_query($db, $log_sql);
-                        //echo "Attack hit! " . $enemy_stats['username'] . " has " . $enemy_stats['HP'] . " HP left.";
+                        $enemy_stats = mysqli_fetch_assoc($usr_check);
                         if ($enemy_stats['HP'] == 0) {
                             echo "Enemy defeated! +100 XP";
                             $xp_update = "UPDATE users SET XP=XP + 100 WHERE iduser='" . $users['iduser'] . "'";
